d0b3c8def53ac66cdd5adc5ab5b64c649b391e18
Karchensky <bryankarchensky@gmail.com>
2023-05-29 14:29:51 -0400

    turned this baby into a web app

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, send_file, make_response
+import io
+import pandas as pd
+from cms_data_scraper import cms_data_scraper
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    uuid_list = request.form.get('uuid_list')
+    uuid_list = [uuid.strip() for uuid in uuid_list.split(',')]
+
+    # Combine CMS data for each UUID into a single DataFrame
+    cms_data_combined = pd.DataFrame()
+    for uuid in uuid_list:
+        scraper = cms_data_scraper(uuid)
+        cms_data = scraper.scraper_cms()
+        cms_data_combined = pd.concat([cms_data_combined, cms_data], ignore_index=True)
+
+    # Generate CSV file from the combined data
+    csv_data = cms_data_combined.to_csv(index=False)
+
+    # Create an in-memory file object
+    csv_io = io.StringIO()
+    csv_io.write(csv_data)
+    csv_io.seek(0)
+
+    # Set response headers to download the file
+    response = make_response(csv_io.getvalue())
+    response.headers['Content-Type'] = 'text/csv'
+    response.headers['Content-Disposition'] = 'attachment; filename=cms_data.csv'
+
+    return response
+
+if __name__ == '__main__':
+    app.run()
--- readme.md
+++ readme.md
@@ -4,14 +4,8 @@
 
 * Repo designed to:
 
-    1) Scrape the CMS data from the https://data.cms.gov/ API.
-    2) Output is stored in a local database. User can set the path.
+    1) Scrape the CMS data from the <https://data.cms.gov/> API.
+    2) Create basic webpage for users to submit a list of UUIDs for scraping.
+    3) Generate a downloadable CSV file with all relevant data from provided UUID list.
 
 ----
-
-* Notes to use this repo:
-
-    1) Everything is written in Python. The packages that are used are: sqlite3, pandas, json, requests, datetime.
-    2) Set your database path (local) and set your UUID list in the cms_data.py file.
-
----
